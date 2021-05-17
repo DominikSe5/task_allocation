213dd47d5789b810b25c083f01b6f57c85d19096
DominikSe5 <76114388+DominikSe5@users.noreply.github.com>
2021-05-17 19:53:31 +0200

    Update agent3.py

--- agent3.py
+++ agent3.py
@@ -7,24 +7,20 @@ class agent3(object):
     def __init__(self):
         self.M = ['funkcija2', 'funkcija3']
         self.gamma = [-0.1, 0.1]
-        self.ready_check = []
-        self.pub = rospy.Publisher("/varijabla_funkciji", poruka, queue_size = 1)
+        self.pub = rospy.Publisher("/varijabla_funkciji", poruka, queue_size = 1, latch=True)
         self.sub = rospy.Subscriber("/varijabla3/funkcija_varijabli", poruka, self.callback, queue_size = 10)
-        self.Rs = {'funkcija2': 0, 'funkcija3': 0}
+        self.Rs = {'funkcija2': [0, 0], 'funkcija3': [0, 0]}
         rospy.spin()
 
     def callback(self, data):
-        self.ready_check.append(data.posiljatelj)
+        print("Agent3 je primio poruku")
         self.Rs[data.posiljatelj] = data.data
-        check = set(self.M) <= set(self.ready_check)
-        if check == True:
-            for funkcija_kojim_saljemo in self.M:
-                poruka_funkciji = poruka()
-                poruka_funkciji.posiljatelj = 'varijabla3'
-                poruka_funkciji.primatelj = funkcija_kojim_saljemo
-                poruka_funkciji.data = self.Poruka_v_f(funkcija_kojim_saljemo)
-                self.pub.publish(poruka_funkciji)
-            self.ready_check.clear()
+        for funkcija_kojim_saljemo in self.M:
+            poruka_funkciji = poruka()
+            poruka_funkciji.posiljatelj = 'varijabla3'
+            poruka_funkciji.primatelj = funkcija_kojim_saljemo
+            poruka_funkciji.data = self.Poruka_v_f(funkcija_kojim_saljemo)
+            self.pub.publish(poruka_funkciji)
 
     def Poruka_v_f(self, f):
         if f in self.M:
