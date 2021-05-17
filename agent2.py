ce84f11b4845194c0b8a08d3cf7206a363145281
DominikSe5 <76114388+DominikSe5@users.noreply.github.com>
2021-05-17 19:38:03 +0200

    Update agent2.py

--- agent2.py
+++ agent2.py
@@ -3,21 +3,21 @@
 import rospy
 from task_allocation.msg import poruka
 
-class agent2(object):
+class agent3(object):
     def __init__(self):
-        self.M = ['funkcija1', 'funkcija2', 'funkcija3']
+        self.M = ['funkcija2', 'funkcija3']
         self.gamma = [-0.1, 0.1]
         self.pub = rospy.Publisher("/varijabla_funkciji", poruka, queue_size = 1, latch=True)
-        self.sub = rospy.Subscriber("/varijabla2/funkcija_varijabli", poruka, self.callback, queue_size = 10)
-        self.Rs = {'funkcija1': [0, 0], 'funkcija2': [0, 0], 'funkcija3': [0, 0]}
+        self.sub = rospy.Subscriber("/varijabla3/funkcija_varijabli", poruka, self.callback, queue_size = 10)
+        self.Rs = {'funkcija2': [0, 0], 'funkcija3': [0, 0]}
         rospy.spin()
 
     def callback(self, data):
-        print("Agent2 je primio poruku")
+        print("Agent3 je primio poruku")
         self.Rs[data.posiljatelj] = data.data
         for funkcija_kojim_saljemo in self.M:
             poruka_funkciji = poruka()
-            poruka_funkciji.posiljatelj = 'varijabla2'
+            poruka_funkciji.posiljatelj = 'varijabla3'
             poruka_funkciji.primatelj = funkcija_kojim_saljemo
             poruka_funkciji.data = self.Poruka_v_f(funkcija_kojim_saljemo)
             self.pub.publish(poruka_funkciji)
@@ -32,8 +32,8 @@ class agent2(object):
         return out
 
 if __name__ == "__main__":
-    rospy.init_node("agent2")
+    rospy.init_node("agent3")
     try:
-        node = agent2()
+        node = agent3()
     except rospy.ROSInterruptException:
         pass
