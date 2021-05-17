63d2120027c1912c06b2bc1dd22f170d2191b113
DominikSe5 <76114388+DominikSe5@users.noreply.github.com>
2021-05-17 19:53:11 +0200

    Update agent2.py

--- agent2.py
+++ agent2.py
@@ -3,137 +3,37 @@
 import rospy
 from task_allocation.msg import poruka
 
-class main_hub(object):
+class agent2(object):
     def __init__(self):
-        self.N = {
-            'funkcija1' : ['varijabla1', 'varijabla2'],
-            'funkcija2' : ['varijabla1', 'varijabla2', 'varijabla3'],
-            'funkcija3' : ['varijabla2', 'varijabla3']
-            }
-        self.M = {
-            'varijabla1' : ['funkcija1', 'funkcija2'],
-            'varijabla2' : ['funkcija1', 'funkcija2', 'funkcija3'],
-            'varijabla3' : ['funkcija2', 'funkcija3']
-            }
-        self.gamma = {
-            'funkcija1' : [0.1, -0.1],
-            'funkcija2' : [-0.1, 0.1],
-            'funkcija3' : [-0.1, 0.1]
-            }
-        ready = 1
-        self.Qs = {
-            'varijabla1, funkcija1': [0, 0], 'varijabla1, funkcija2': [0, 0], ## posiljatelj, primatelj: [poruka]
-            'varijabla2, funkcija1': [0, 0], 'varijabla2, funkcija2': [0, 0], 'varijabla2, funkcija3': [0, 0],
-            'varijabla3, funkcija2': [0, 0], 'varijabla3, funkcija3': [0, 0]
-            }
-        self.received = {
-            'varijabla1' : ['funkcija1', 'funkcija2'],
-            'varijabla2' : ['funkcija1', 'funkcija2', 'funkcija3'],
-            'varijabla3' : ['funkcija2', 'funkcija3']
-            }
+        self.M = ['funkcija1', 'funkcija2', 'funkcija3']
+        self.gamma = [-0.1, 0.1]
+        self.pub = rospy.Publisher("/varijabla_funkciji", poruka, queue_size = 1, latch=True)
+        self.sub = rospy.Subscriber("/varijabla2/funkcija_varijabli", poruka, self.callback, queue_size = 10)
+        self.Rs = {'funkcija1': [0, 0], 'funkcija2': [0, 0], 'funkcija3': [0, 0]}
+        rospy.spin()
 
-        sub = rospy.Subscriber('/varijabla_funkciji', poruka, self.callback, queue_size=1)
+    def callback(self, data):
+        print("Agent2 je primio poruku")
+        self.Rs[data.posiljatelj] = data.data
+        for funkcija_kojim_saljemo in self.M:
+            poruka_funkciji = poruka()
+            poruka_funkciji.posiljatelj = 'varijabla2'
+            poruka_funkciji.primatelj = funkcija_kojim_saljemo
+            poruka_funkciji.data = self.Poruka_v_f(funkcija_kojim_saljemo)
+            self.pub.publish(poruka_funkciji)
 
-        self.pubs = {}
-        for varijabla in self.received:
-            self.pubs[varijabla] = rospy.Publisher('/{}/funkcija_varijabli'.format(varijabla), poruka, queue_size=1, latch=True)
-
-        rospy.sleep(3)
-        
-        while not rospy.is_shutdown():
-            rospy.sleep(5)
-            print(self.received)
-            check = []
-            for varijabla in self.received:
-                if set(self.M[varijabla]) <= set(self.received[varijabla]):
-                    check.append(True)
-            if all(check) and len(check) != 0:
-                for funkcija in self.gamma:
-                    varijable_kojima_saljemo = self.N[funkcija]
-                    for varijabla in varijable_kojima_saljemo:
-                        msg = poruka()
-                        msg.primatelj = varijabla                       
-                        msg.posiljatelj = funkcija
-                        msg.data = self.Poruka_f_v(varijabla, funkcija)
-                        self.pubs[varijabla].publish(msg)
-                        self.received[varijabla].clear()
-
-    def calc_U(self, f):
-        xor_sum = []
-        U = []
-        N = self.N[f]
-        gamma = self.gamma[f]
-        bits = {}
-        i1 = 0
-        i2 = 0
-        i3 = 0
-        temp = list(self.N.keys())
-        m_num = temp.index(f) + 1
-        m = 'varijabla{}'.format(m_num)
-        while i1 != pow(2, len(N)):            
-            xor_sum.append(0) ## stvaranje liste [0..0] velicine 2^len(N)
-            temp = [1 if i1 & (1 << (7-n)) else 0 for n in range(8)] ##varijabla u koju spremam binarni oblik countera i
-            while len(temp) != len(N):
-                if temp[0] == 0:
-                    del temp[0] ## u gornjem koraku za npr. i=5, temp ima oblik [0,0,0,0,0,1,0,1], nakon ovog koraka micu se sve nule na lijevoj strani sve dok temp nije velicine N
-            bits[i1] = temp ## dictinoary sa svim temp-ovima, npr: len(N) == 2 => bits{0 : [0, 0], 1 : [0, 1], 2 : [1, 0], 3 : [1, 1]}
-            i1 = i1 + 1
-        while i2 != pow(2, len(N)):
-            temp = bits[i2]
-            for j in N:
-                if j != m:
-                    xor_sum[i2] += self.calc_XOR(temp[N.index(j)], temp[N.index(m)]) ##racunanje svih xorova za kombinacije varijabli 
-            i2 += 1
-        while i3 != pow(2, len(N)):
-            temp = bits[i3]
-            if temp[N.index(m)] == 0:
-                U.append(gamma[0] - xor_sum[i3])
-            else:
-                U.append(gamma[1] - xor_sum[i3])
-            i3 += 1
-        return U
-    
-    def calc_XOR(self, x, y):
-        if x == y: return 1
-        else: return 0
-    
-    def Poruka_f_v(self, v, f):
-        # sum_Qs = {}
-        # for i in self.N[f]:
-        #     if i != v:
-        #         sum_Qs[i] = self.Qs['{}, {}'.format(i, f)]
-        U_0 = []
-        U_1 = []
-        i1 = 0
-        output = [0, 0]
-        U = self.calc_U(f)
-        N = self.N[f]
-        while i1 != pow(2, len(N)):            
-            temp = [1 if i1 & (1 << (7-n)) else 0 for n in range(8)] ##pogledaj funkciju calc_U za objasnjenje ove sekcije koda --
-            while len(temp) != len(N): ## --
-                if temp[0] == 0: ## --
-                    del temp[0] ## --
-            # for i2 in sum_Qs:
-            #     if temp[N.index(i2)] == 0:
-            #         U[i1] += sum_Qs[i2][0]
-            #     else:
-            #         U[i1] += sum_Qs[i2][1]
-            if temp[N.index(v)] == 0: ## razvrstavanje vrijednosti funkcije U na dvije liste, u jednu idu vrijednosti za koje varijabla kojoj saljem poruku ima vrijednost 0, a u drugu za vrijednosti 1
-                U_0.append(U[i1])
-            elif temp[N.index(v)] == 1:
-                U_1.append(U[i1])   
-            i1 = i1 + 1  
-        output[0] += max(U_0)   
-        output[1] += max(U_1)  
-        return output
-    
-    def callback(self, data): 
-        self.Qs['{}, {}'.format(data.posiljatelj, data.primatelj)] = data.data
-        self.received[data.posiljatelj].append(data.primatelj)
+    def Poruka_v_f(self, f):
+        if f in self.M:
+            out = [0, 0]
+            for primatelj, vrijednost in self.Rs.items():
+                if primatelj != f:
+                    out[0] += vrijednost[0]
+                    out[1] += vrijednost[1]
+        return out
 
 if __name__ == "__main__":
-    rospy.init_node("Main_Hub")
+    rospy.init_node("agent2")
     try:
-        node = main_hub()
+        node = agent2()
     except rospy.ROSInterruptException:
         pass
